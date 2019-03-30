5df3282853a84ca545242921c0b30e4709f587e6
Luke Taaffe <ltaaffe@names.co.uk>
2019-03-30 18:46:09 +0000

    Water container add water test

--- src/tests/TestWaterContainer.php
+++ src/tests/TestWaterContainer.php
@@ -3,8 +3,25 @@
 namespace SoConnect\Coffee\Tests;
 
 use PHPUnit\Framework\TestCase;
+use SoConnect\Espresso\Containers\WaterContainer\WaterContainer;
 
 class TestWaterContainer extends TestCase
 {
+    /**
+     * @test
+     * @covers \SoConnect\Coffee\Containers\BeanContainer\BeanContainer::addWater
+     */
+    public function testWaterContainerCanAddWater(): void
+    {
+        $waterContainer = new WaterContainer(10.0);
 
+        $waterContainer->addWater(5.4);
+        $this->assertEquals(15.4, $waterContainer->getWater());
+
+        $waterContainer->addWater(2);
+        $this->assertEquals(17.4, $waterContainer->getWater());
+
+        $waterContainer->addWater(1000);
+        $this->assertEquals(1017.4, $waterContainer->getWater());
+    }
 }
