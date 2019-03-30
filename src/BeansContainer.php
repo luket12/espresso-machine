463a23e28fff43224d91e12605aefb3eddbcd1d1
Luke Taaffe <ltaaffe@names.co.uk>
2019-03-30 15:01:06 +0000

    Missed interface

---
+++ src/BeansContainer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SoConnect\Espresso;
+
+use SoConnect\Espresso\Exceptions\ContainerFullException;
+
+interface BeansContainer
+{
+
+    /**
+     * Adds beans to the container
+     *
+     * @param integer $numSpoons number of spoons of beans
+     * @throws ContainerFullException
+     *
+     * @return void
+     */
+    public function addBeans(int $numSpoons) : void;
+
+    /**
+     * Get $numSpoons from the container
+     *
+     * @param integer $numSpoons number of spoons of beans
+     * @return integer
+     */
+    public function useBeans(int $numSpoons) : int;
+
+    /**
+     * Returns the number of spoons of beans left in the container
+     *
+     * @return integer
+     */
+    public function getBeans() : int;
+}
